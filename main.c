8e4635fba72b998f0ec71670d59592893c1d2495
Garrett Quintero <garrett.quintero@gmail.com>
2019-04-25 15:09:43 -0600

    now we can do temperature and relative humidity

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include "mcc_generated_files/mcc.h"
 
 #define TMPHUM_ADDR 0b1000000   // 7-bit i2c address for temp & humidity sensor
-#define MEAS_TEMP   0xF3        // NO HOLD MASTER read temp command code
-#define MEAS_RH     0xF5        // NO HOLD - read Relative Humidity command code
-#define WRITE_RHT   0xE6        // Write to register
-#define READ_RHT    0xE7        // Read from user register
+#define MEAS_TEMP   0xE3        // NO HOLD MASTER read temp command code
+#define MEAS_RH     0xE5        // NO HOLD - read Relative Humidity command code
+//#define WRITE_RHT   0xE6        // Write to register
+//#define READ_RHT    0xE7        // Read from user register
 
 void printHelp();
-uint16_t measureTemp();
-//uint16_t readTemp();
-
+int16_t measureTemp();
+int16_t measureRH();
 uint16_t RHT_value = 0x00;
 
 
@@ -19,7 +18,8 @@ uint16_t RHT_value = 0x00;
 void main(void)
 {
     char cmd;
-    
+    int16_t temp;
+    int16_t relative;
     // Initialize the device
     SYSTEM_Initialize();
     INTERRUPT_PeripheralInterruptEnable();
@@ -54,12 +54,26 @@ void main(void)
                     
                 /** READ TEMP */
                 case 't' :
-                    printf("Collecting Temperature Value...\r\n");
+                    printf("Collecting Temperature value...\r\n");
                     RHT_value = measureTemp();
                     
-                    printf("Read %x from Temp Sensor\r\n", RHT_value);
+                    printf("Read %X from Temp Sensor\r\n", RHT_value);
+                    
+                    temp = ((175.72 * RHT_value)/(65536)) - 46.85;
+                    
+                    printf("Estimated temperature is: %i\r\n", temp);
+                    
                     break;
                 /** READ Relative Humidity */
+                case 'r' :
+                    printf("Collecting Relative Humidity value...\r\n");
+                    RHT_value = measureRH();
+                    
+                    relative = ((125.00*RHT_value)/(65536)) - 6;
+                    printf("Estimated Relative Humidity: %i\r\n", relative);
+                    
+                    break;
+                    
                 default:
                     printf("Unknown Key %c\r\n", cmd);
                     break;
@@ -68,26 +82,54 @@ void main(void)
     }
 }
 
-uint16_t measureTemp() {
-        uint16_t value;
-    uint8_t data[2] = {MEAS_TEMP};
-    I2C1_MESSAGE_STATUS I2C_Wflag;
+int16_t measureTemp() {
+    int16_t value;
+    uint8_t data[4] = {MEAS_TEMP, MEAS_TEMP, 0, 0};
+    I2C2_MESSAGE_STATUS I2C_Wflag;
 
-    I2C_Wflag = I2C1_MESSAGE_PENDING;
-    I2C1_MasterWrite(data, 1, TMPHUM_ADDR, &I2C_Wflag);
-//    while(I2C_Wflag == I2C1_MESSAGE_PENDING);
-
+    //tell the sensor to measure
+    I2C_Wflag = I2C2_MESSAGE_PENDING;
+    I2C2_MasterWrite(data, 1, TMPHUM_ADDR, &I2C_Wflag);
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
     
-//    I2C1_MESSAGE_STATUS I2C_Wflag = I2C1_MESSAGE_PENDING;
-    do {
-        printf("Starting read on %x\r\n", TMPHUM_ADDR);
-        I2C1_MasterRead(data, 2, TMPHUM_ADDR, &I2C_Wflag);
-        printf("Current i2c flag : %x", I2C_Wflag);
-    } while(I2C_Wflag == I2C1_MESSAGE_PENDING);
     
+    // read from the sensor
+    I2C_Wflag = I2C2_MESSAGE_PENDING;
+    I2C2_MasterRead(data, 4, TMPHUM_ADDR, &I2C_Wflag);
+    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
+    
+    
+    printf("MSB: %X, LSB: %X\r\n", data[0], data[1]);
+    // convert to a 16 bit value
     value = data[0];
     value = value << 8;
-    value = value + data[1];
+    value = value | data[1];
+    
+    return value;
+}
+
+int16_t measureRH() {
+    int16_t value;
+    uint8_t data[4] = {MEAS_RH, MEAS_RH, 0, 0};
+    I2C2_MESSAGE_STATUS I2C_Wflag;
+
+    //tell the sensor to measure
+    I2C_Wflag = I2C2_MESSAGE_PENDING;
+    I2C2_MasterWrite(data, 1, TMPHUM_ADDR, &I2C_Wflag);
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+    
+    
+    // read from the sensor
+    I2C_Wflag = I2C2_MESSAGE_PENDING;
+    I2C2_MasterRead(data, 4, TMPHUM_ADDR, &I2C_Wflag);
+    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
+    
+    
+    printf("MSB: %X, LSB: %X\r\n", data[0], data[1]);
+    // convert to a 16 bit value
+    value = data[0];
+    value = value << 8;
+    value = value | data[1];
     
     return value;
 }
