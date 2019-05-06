b386ebab7b0d1efc92eaf100c1ce0f34fd76acf3
MumblingCompSci <garrett.quintero@gmail.com>
2019-05-06 00:11:30 -0600

    Update main.c

--- main.c
+++ main.c
@@ -12,29 +12,27 @@
 #define LIGHT_CONTROL   0xA0        // set light to control register
 #define LIGHT_TIMING    0xA1        // timing register
 
-void printHelp();
 int16_t measureTemp();
 int16_t measureRH();
 void turnOnALS();
 uint16_t readALS();
 uint8_t readMoisture();
 
-
 /*
                          Main application
  */
-void main(void)
-{
-    uint16_t RHT_value = 0x00;
+void main(void) {
     char cmd;
-    int16_t temp;
-    int16_t relative;
-    int16_t lightLevel;
+    uint16_t temperature;
+    uint16_t humidity;
+    uint16_t light;
+    uint8_t moisture;
+
     // Initialize the device
     SYSTEM_Initialize();
     INTERRUPT_PeripheralInterruptEnable();
     INTERRUPT_GlobalInterruptEnable();
-    
+
     printf("Development Board\r\n");
     printf("Plant Monitor Terminal\r\n");
     printf("Component Testing\r\n");
@@ -42,67 +40,50 @@ void main(void)
 
     // activate ambient light sensor
     turnOnALS();
-    
+
+
+
     for (;;) {
         if (EUSART2_DataReady) {
-            cmd = EUSART2_Read();
-            
-            switch(cmd) {
-                case '?' :
-                    printHelp();
-                    break;
-                case 'z' :
-                    for (int i = 0; i < 40; i++)
-                        printf("\r\n");
-                    break;
-                case 'Z' :
-                    for (int i = 0; i < 40; i++)
-                        printf("\r\n");
-                    RESET();
-                    break;
-                case 'o' :
-                    printf("o:    ok\r\n");
-                    break;
-                    
-                    
-                /** READ TEMP */
-                case 't' :
-                    printf("\r\nCollecting Temperature value...\r\n");
-                    RHT_value = measureTemp();
-                    
-                    printf("Read %X from Temp Sensor\r\n", RHT_value);
-                    
-                    temp = ((175.72 * RHT_value)/(65536)) - 46.85;
-                    
-                    printf("Estimated temperature is: %i\r\n", temp);
-                    
-                    break;
-                /** READ Relative Humidity */
-                case 'r' :
-                    printf("\r\nCollecting Relative Humidity value...\r\n");
-                    RHT_value = measureRH();
-                    
-                    relative = ((125.00*RHT_value)/(65536)) - 6;
-                    printf("Estimated Relative Humidity: %i\r\n", relative);
-                    
-                    break;
-                    
-                /** Read Ambient Light */
-                case 'L' :
-                    printf("\r\nCollecting Ambient Light Level value...\r\n");
-                    
-                    lightLevel = readALS();
-                    printf("Ambient Light Level: %u\r\n", lightLevel);
-                    break;
-                case 'm':
-                    printf("\r\nReading Soil Moisture...\r\n");
-                    printf("Soil Moisture Sensor Value: %u\r\n", readMoisture());
-                    break;
+            cmd = EUSART2_Read(); // will not be used TODO remove
 
-                default:
-                    printf("Unknown Key %c\r\n", cmd);
-                    break;
-            }
+            printf("got cmd %u\r\n", cmd);
+
+            while (!EUSART2_is_tx_ready());
+
+            printf("gathering data\r\n");
+            humidity = measureRH();
+            light = readALS();
+            moisture = readMoisture();
+            temperature = measureTemp();
+            
+            printf("Writing values\r\n");
+
+            printf("humidity: %x\r\n", humidity);
+            EUSART2_Write(humidity);
+            //EUSART2_Write(humidity >> 8);
+            //EUSART2_Write(humidity & 0xFF);
+
+            printf("light: %x\r\n", light);
+            EUSART2_Write(light);
+            //EUSART2_Write(light >> 8);
+            //EUSART2_Write(light & 0xFF);
+
+            printf("moisture: %x\r\n", moisture);
+            EUSART2_Write(moisture);
+
+            printf("temperature: %x\r\n", temperature);
+            EUSART2_Write(temperature);
+            //EUSART2_Write(temperature >> 8);
+            //EUSART2_Write(temperature & 0xFF);
+            
+            EUSART2_Write('\n');
+
+            while (!EUSART2_is_tx_done());
+            printf("Wrote values\r\n\r\n");
+
+            //temp = ((175.72 * RHT_value)/(65536)) - 46.85;
+            //relative = ((125.00*RHT_value)/(65536)) - 6;
         }
     }
 }
@@ -116,20 +97,20 @@ int16_t measureTemp() {
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterWrite(data, 1, TMPHUM_ADDR, &I2C_Wflag);
     while (I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
-    
+
+
     // read from the sensor
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterRead(data, 4, TMPHUM_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
-    
-    printf("MSB: %X, LSB: %X\r\n", data[0], data[1]);
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
+
+    //printf("MSB: %X, LSB: %X\r\n", data[0], data[1]);
     // convert to a 16 bit value
     value = data[0];
     value = value << 8;
     value = value | data[1];
-    
+
     return value;
 }
 
@@ -142,102 +123,89 @@ int16_t measureRH() {
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterWrite(data, 1, TMPHUM_ADDR, &I2C_Wflag);
     while (I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
-    
+
+
     // read from the sensor
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterRead(data, 4, TMPHUM_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
-    
-    printf("MSB: %X, LSB: %X\r\n", data[0], data[1]);
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
+
+    //printf("MSB: %X, LSB: %X\r\n", data[0], data[1]);
     // convert to a 16 bit value
     value = data[0];
     value = value << 8;
     value = value | data[1];
-    
+
     return value;
 }
 
 void turnOnALS() {
-    uint8_t data[1] = { LIGHT_CONTROL };
-    
+    uint8_t data[1] = {LIGHT_CONTROL};
+
     I2C2_MESSAGE_STATUS I2C_Wflag = I2C2_MESSAGE_PENDING;
-    
+
     // tell it we're writing to the control register
     I2C2_MasterWrite(data, 1, LIGHT_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
     // write to the control register
     data[0] = 0x03; //turn sensor on
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterWrite(data, 1, LIGHT_ADDR, &I2C_Wflag);
     while (I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
+
     // tell it we're writing to the timing register
     data[0] = LIGHT_TIMING;
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterWrite(data, 1, LIGHT_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
     // write to the timing register
     data[0] = 0x02;
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterWrite(data, 1, LIGHT_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
     // verify the timing register
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterRead(data, 1, LIGHT_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
     if (data[0] == 0x02) printf("Light sensor active\r\n");
     else printf("Light sensor did not turn on\r\n");
 }
 
 uint16_t readALS() {
     // tell the sensor to read from data1 lower
-    uint8_t data[2] = { LIGHT_DATALOW, 0x00 };
+    uint8_t data[2] = {LIGHT_DATALOW, 0x00};
     uint16_t value;
     I2C2_MESSAGE_STATUS I2C_Wflag = I2C2_MESSAGE_PENDING;
-    
+
     // tell it we're writing to the DATA1LOW register
     I2C2_MasterWrite(data, 1, LIGHT_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
     //read data
     I2C_Wflag = I2C2_MESSAGE_PENDING;
     I2C2_MasterRead(data, 2, LIGHT_ADDR, &I2C_Wflag);
-    while(I2C_Wflag == I2C2_MESSAGE_PENDING);
-    
+    while (I2C_Wflag == I2C2_MESSAGE_PENDING);
+
     value = data[1];
     value = value << 8;
     value = value | data[0];
-    
+
     return value;
 }
 
 uint8_t readMoisture() {
-  ADCON0bits.GO_NOT_DONE = 1;
-  return ADRESH;
-}
-
-void printHelp() {
-    printf("\r\n--------------------\r\n");
-    printf("--------------------\r\n");
-    printf("o: k\r\n");
-    printf("Z: reset processor\r\n");
-    printf("z: clear terminal\r\n");
-    printf("r: get relative humidity\r\n");
-    printf("t: get temperature\r\n");
-    printf("L: get ambient light level\r\n");
-    printf("m: get soil moisture\r\n");
-    printf("--------------------\r\n");
+    ADCON0bits.GO_NOT_DONE = 1;
+    return ADRESH;
 }
 
 void TMR0_DefaultInterruptHandler(void) {
-    
+
 }
 /**
  End of File
-*/
+ */
