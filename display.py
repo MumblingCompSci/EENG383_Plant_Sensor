cca617f094c11f57c4644fb67630c92b82c898ef
Brandon Verkamp <jadelclemens@gmail.com>
2019-05-06 03:33:58 +0000

    Draw bars as 100px wide rectangles, make temperature text larger

--- display.py
+++ display.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFont, ImageDraw
 
 # Constants
 DISPLAY_DIMS = (212, 104)
-LINE_SPACING = 20
+LINE_SPACING = 31
 WARNING_LEVEL = {'T': 50,
            'H': 80,
            'L': 90,
@@ -12,6 +12,9 @@ WARNING_LEVEL = {'T': 50,
 # Get the color (black for normal, red for "warning") of the display text
 def getColor(param, params, warning):
     return InkyPHAT.BLACK if params[param] < warning[param] else InkyPHAT.RED
+
+def addCoords(pos1, pos2):
+    return tuple(map(sum, zip(pos1, pos2)))
 
 # Test info. I'll assume a 0-100 scale for now
 params = {'T': 74, #temperature
@@ -24,27 +27,44 @@ display = InkyPHAT('red')
 display.set_border(InkyPHAT.BLACK)
 
 # Create image and font stuff
-fontRegular = ImageFont.truetype("./fonts/RobotoMono-Regular.ttf", 15)
-fontBold = ImageFont.truetype("./fonts/RobotoMono-Bold.ttf", 15)
-fontTemp = ImageFont.truetype("./fonts/RobotoMono-Bold.ttf", 25)
+fontRegular = ImageFont.truetype("./fonts/RobotoMono-Regular.ttf", 25)
+fontBold = ImageFont.truetype("./fonts/RobotoMono-Bold.ttf", 25)
+fontTemp = ImageFont.truetype("./fonts/Roboto-Bold.ttf", 36)
 img = Image.new('P', DISPLAY_DIMS, InkyPHAT.WHITE)
+# img = Image.open("./bg/inkbg.png")
 
 # Draw example text on image
 txtLayer = ImageDraw.Draw(img)
+
+# Start and end of "progress bar"
 start = ['[']
 stop = [']']
 
 # Draw progress bars for all params but temperature
 for num,param in enumerate(params, start=-1):
+    # Skip drawing a bar for temperature: We use text for this
     if param == 'T':
         continue
-    label = [param]
-    bar = ['|' for i in range(int(params[param]/10))]
-    dots = ['.' for i in range(int(params[param]/10), 10)]
-    progress = label + start + bar + dots + stop
-    txtLayer.text((10,10+LINE_SPACING*num), ''.join(progress), font=fontBold, fill=getColor(param, params, WARNING_LEVEL))
 
-txtLayer.text((150,35), str(params['T']) + u'\N{DEGREE SIGN}' + 'C', font=fontTemp, fill=getColor('T', params, WARNING_LEVEL))
+    startpos = (10, 5+LINE_SPACING*num)
+    fillColor = getColor(param, params, WARNING_LEVEL)
+
+    # Draw progress bar label
+    txtLayer.text(startpos, param, font=fontBold, fill=fillColor)
+
+    # Draw the progress bar bounding rectangles
+    rectPos = addCoords(startpos, (20, 8))
+    # To achieve an 100px wide rect, we add 102 to the end coords
+    # 2px compensation for each 1px wide border, 1px for rectangle() behavior
+    # see https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html
+    txtLayer.rectangle([rectPos, addCoords(rectPos, (103, 18))], InkyPHAT.WHITE, InkyPHAT.BLACK, 1)
+
+    # Draw the actual progress rectangles
+    rectPos = addCoords(rectPos, (1,1)) # make sure we draw _inside_ the bounding rect
+    txtLayer.rectangle([rectPos, addCoords(rectPos, (params[param]+1, 16))], fill=fillColor)
+
+fillColor = getColor('T', params, WARNING_LEVEL)
+txtLayer.text((150, 20), str(params['T']) + '\n' + u'\N{DEGREE SIGN}' + 'C', font=fontTemp, fill=fillColor, align='center', spacing=-5)
 
 # Set the image to display, and display it!
 display.set_image(img)
